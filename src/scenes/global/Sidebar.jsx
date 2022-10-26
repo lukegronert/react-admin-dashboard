f79ba444c910ca94971e722d016230dcd6225b32
Luke Gronert <lukegronert@gmail.com>
2022-10-26 01:42:48 -0700

    Copy pasted code that was skipped in video

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -23,8 +23,55 @@ const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [selected, setSelected] = useState("Dashboard");
   return (
-    <div>Sidebar</div>
-  )
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`
+        },
+        '& .pro-icon-wrapper': {
+          backgroundColor: 'transparent !important'
+        },
+        '& .pro-inner-item': {
+          padding: '5px 35px 5px 20px !important'
+        },
+        '& .pro-inner-item:hover': {
+          color: "#868dfb !important"
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important"
+        }
+      }}>
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+                >
+                  <Typography variant="h3" color={colors.grey[100]}>
+                    ADMINIS
+                  </Typography>
+                  <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                    <MenuOutlinesIcon />
+                  </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
 }
 
 export default Sidebar;
