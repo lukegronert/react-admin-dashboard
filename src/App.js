d7ff10faf8b3e1eb26792e66aea4fa7a47653177
Luke Gronert <lukegronert@gmail.com>
2022-10-26 01:14:52 -0700

    Added routes to App.js. Commented out routes that don't exist yet, but will in the future

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import { ColorModeContext, useMode, } from './theme';
 //CssBaseline resets the css to a baseline
 // ThemeProvider allows us to pass themes into mui
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+// import Team from './scenes/team';
+// import Invoices from './scenes/invoices';
+// import Contacts from './scenes/contacts';
+// import Bar from './scenes/bar';
+// import Form from './scenes/form';
+// import Line from './scenes/line';
+// import Pie from './scenes/pie';
+// import FAQ from './scenes/faq';
+// import Geography from './scenes/geography';
+// import Calendar from './scenes/calendar';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -11,7 +25,22 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="app">
-        <main className="content"></main>
+        <main className="content">
+          <Topbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            {/* <Route path="/team" element={<Team />} /> */}
+            {/* <Route path="/invoices" element={<Contacts />} /> */}
+            {/* <Route path="/contacts" element={<Invoices />} /> */}
+            {/* <Route path="/bar" element={<Bar />} /> */}
+            {/* <Route path="/form" element={<Form />} /> */}
+            {/* <Route path="/line" element={<Line />} /> */}
+            {/* <Route path="/pie" element={<Pie />} /> */}
+            {/* <Route path="/faq" element={<FAQ />} /> */}
+            {/* <Route path="/geography" element={<Geography />} /> */}
+            {/* <Route path="/calendar" element={<Calendar />} /> */}
+          </Routes>
+        </main>
       </div>
     </ThemeProvider>
   </ColorModeContext.Provider>
