9648a0664f12bea01bd3893d22fc7cc3317ee136
Luke Gronert <lukegronert@gmail.com>
2022-10-27 23:37:50 -0700

    Created Team scene, styled DataGrid, and added info from MockDataTeam

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
-// import Team from './scenes/team';
+import Team from './scenes/team';
 // import Invoices from './scenes/invoices';
 // import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
@@ -30,7 +30,7 @@ function App() {
           <Topbar />
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            {/* <Route path="/team" element={<Team />} /> */}
+            <Route path="/team" element={<Team />} />
             {/* <Route path="/invoices" element={<Contacts />} /> */}
             {/* <Route path="/contacts" element={<Invoices />} /> */}
             {/* <Route path="/bar" element={<Bar />} /> */}
