385e23494ebc22afd3d1364e78fe051e80c8fcc9
Luke Gronert <lukegronert@gmail.com>
2022-10-30 18:09:08 +0800

    Added FullCalendar to Calendar component. FullCalendar now displays events on the calendar and on the sidebar. Users can create and delete events.

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -85,7 +85,37 @@ const Calendar = () => {
             })}
           </List>
         </Box>
-
+        {/* CALENDAR */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin
+            ]}
+            headerToolbar={{
+              left: "prev, next today",
+              center: "title",
+              right: "dayGridMonth, timeGridWeek, timeGridDay, listMonth"
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => {
+              setCurrentEvents(events)
+            }}
+            initialEvents={[
+              { id: "1234", title: "All-day event", date: "2022-09-14"},
+              { id: "4321", title: "Timed event", date: "2022-09-28"}
+            ]}
+          />
+        </Box>
       </Box>
     </Box>
   )
