0bbb68fd5f221b9f104735a5a500d1455df80789
Luke Gronert <lukegronert@gmail.com>
2022-10-31 15:31:09 +0800

    Finished creating grid layout for Dashboard scene and filled it with StatBox components, charts, and data

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -2,7 +2,7 @@ import { Box, Typography, useTheme } from '@mui/material';
 import { tokens } from '../theme';
 import ProgressCircle from "./ProgressCircle";
 
-const StatBox = ({ title, subtitle, icon, increase }) => {
+const StatBox = ({ title, subtitle, icon, progress, increase }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
@@ -18,14 +18,14 @@ const StatBox = ({ title, subtitle, icon, increase }) => {
         <Box>
           <ProgressCircle progress={progress} />
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="h5" sx={{ color: colors.greenAccent[500]}}>
-            {subtitle}
-          </Typography>
-          <Typography variant="h5" fontWeight="italic" sx={{ color: colors.grey[600]}}>
-            {increase}
-          </Typography>
-        </Box>
+      </Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h5" sx={{ color: colors.greenAccent[500]}}>
+          {subtitle}
+        </Typography>
+        <Typography variant="h5" fontWeight="italic" sx={{ color: colors.grey[600]}}>
+          {increase}
+        </Typography>
       </Box>
     </Box>
   )
